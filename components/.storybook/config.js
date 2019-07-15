8dad100e7191d4c8c416d8b9a049ccd1bc25a7ca
filipdanisko <filip@danisko.cz>
2019-07-15 17:02:40 +0200

    DEV: storybook config fix

---
+++ components/.storybook/config.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+import { configure, addDecorator, addParameters } from "@storybook/react";
+import { withKnobs } from "@storybook/addon-knobs";
+// import "loki/configure-react";
+
+addParameters({
+  options: {
+    panelPosition: "bottom"
+  }
+});
+addDecorator(withKnobs);
+
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+
+function loadStories() {
+  const req = require.context("../src", true, /.stories.js$/);
+  console.log(req);
+  req.keys().forEach(req);
+}
+
+configure(loadStories, module);
