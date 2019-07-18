42a520bcbffd65825769ecfd6d5bd72fd257fafe
filipdanisko <filip@danisko.cz>
2019-07-18 16:54:03 +0200

    Updating hero component

--- components/src/Hero/Hero.stories.js
+++ components/src/Hero/Hero.stories.js
@@ -5,9 +5,47 @@ import { storiesOf } from '@storybook/react';
 
 import heroImg from '../../static/hero.jpg';
 import heroPattern from '../../static/pattern04.svg';
+import NavBar from '../NavBar';
 
 import Hero from './index';
 
-storiesOf('Hero', module).add('Default', () => (
-  <Hero img={heroImg} pattern={heroPattern} />
-));
+storiesOf('Hero', module)
+  .add('Default', () => <Hero img={heroImg} pattern={heroPattern} />)
+  .add('With NavBar', () => (
+    <Hero
+      img={heroImg}
+      pattern={heroPattern}
+      navBar={
+        <NavBar
+          title="Travel Hackathon"
+          items={[
+            {
+              title: 'About',
+              onClick: '',
+              href: '#test',
+            },
+            {
+              title: 'Prizes',
+              onClick: '',
+              href: '#test',
+            },
+            {
+              title: 'FAQ',
+              onClick: '',
+              href: '#test',
+            },
+            {
+              title: 'Contact',
+              onClick: '',
+              href: '#test',
+            },
+            {
+              title: 'Sponsors',
+              onClick: '',
+              href: '#test',
+            },
+          ]}
+        />
+      }
+    />
+  ));
--- components/src/Hero/index.js
+++ components/src/Hero/index.js
@@ -1,12 +1,12 @@
 // @flow
 
 import * as React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Button from '@kiwicom/orbit-components/lib/Button';
 import Heading from '@kiwicom/orbit-components/lib/Heading';
 import Text from '@kiwicom/orbit-components/lib/Text';
 import Stack from '@kiwicom/orbit-components/lib/Stack';
-import Grid from '@kiwicom/orbit-components/lib/utils/Grid';
+import media from '@kiwicom/orbit-components/lib/utils/mediaQuery';
 
 import heroBox from '../../static/pattern05.svg';
 import defaultTheme from '../defaultTheme';
@@ -15,6 +15,7 @@ const StyledHero = styled.div`
   background: black;
   height: 100%;
   height: 100vh;
+  min-height: 790px;
   background-image: ${({ img }) => `url(${img})`};
   background-repeat: no-repeat;
   background-size: cover;
@@ -22,17 +23,19 @@ const StyledHero = styled.div`
   position: relative;
   overflow: hidden;
 
-  &:after {
-    content: '';
-    width: 69px;
-    height: 57px;
-    display: block;
-    background-image: url(${heroBox});
-    background-size: cover;
-    position: absolute;
-    bottom: -19px;
-    right: 20px;
-  }
+  ${media.tablet(css`
+    &:after {
+      content: '';
+      width: 69px;
+      height: 57px;
+      display: block;
+      background-image: url(${heroBox});
+      background-size: cover;
+      position: absolute;
+      bottom: -19px;
+      right: 20px;
+    }
+  `)};
 `;
 
 StyledHero.defaultProps = {
@@ -57,57 +60,90 @@ StyledDate.defaultProps = {
 };
 
 const StyledAlign = styled.span`
-  text-align: right;
+  ${media.mediumMobile(
+    css`
+      text-align: right;
+    `,
+  )}
 `;
-
-const StyledWrapper = styled.div`
-  height: 100%;
-`;
-StyledWrapper.defaultProps = {
+StyledAlign.defaultProps = {
   theme: defaultTheme,
 };
+
+const StyledWrapper = styled.div`
+  /* height: 100%; */
+  flex-basis: 70%;
+`;
 
 const BottomTextWrapper = styled(HeadingWrapper)`
   align-items: flex-end;
   padding-bottom: 4.16vw;
+  flex-basis: 30%;
 `;
 
 const StyledTextWithPattern = styled.span`
   display: block;
   position: relative;
 
-  &:after {
-    content: '';
-    min-width: 190px;
-    min-height: 130px;
-    width: 20vw;
-    height: 14.7vw;
-    display: block;
-    background-image: ${({ pattern }) => `url(${pattern})`};
-    background-size: cover;
-    position: absolute;
-    top: -6vw;
-    right: -7vw;
-  }
+  ${media.tablet(css`
+    &:after {
+      content: '';
+      min-width: 190px;
+      min-height: 130px;
+      width: 20vw;
+      height: 14.7vw;
+      display: block;
+      background-image: ${({ pattern }) => `url(${pattern})`};
+      background-size: cover;
+      position: absolute;
+      top: -6vw;
+      right: -7vw;
+    }
+  `)};
 `;
 
-const Hero = ({ img, pattern }) => {
+StyledTextWithPattern.defaultProps = {
+  theme: defaultTheme,
+};
+
+const StyledNavBarWrapper = styled.div`
+  padding: 4.2vw 0 0 0;
+`;
+
+const StyledSpan = styled.span`
+  word-break: break-all;
+`;
+
+const StyledStack = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const Hero = ({ img, pattern, navBar }) => {
   return (
     <StyledHero img={img}>
-      <Grid rows="calc(100vh - 230px) 230px">
+      <StyledStack>
         <StyledWrapper>
+          {navBar && <StyledNavBarWrapper>{navBar}</StyledNavBarWrapper>}
           <HeadingWrapper>
             <Heading inverted type="display">
-              <StyledTextWithPattern pattern={pattern}>
-                Travel
-              </StyledTextWithPattern>
-              Hackathon
+              <StyledSpan>
+                <StyledTextWithPattern pattern={pattern}>
+                  Travel
+                </StyledTextWithPattern>
+                Hackathon
+              </StyledSpan>
             </Heading>
           </HeadingWrapper>
         </StyledWrapper>
 
         <BottomTextWrapper>
-          <Stack flex direction="row">
+          <Stack
+            flex
+            direction="column-reverse"
+            mediumMobile={{ direction: 'row' }}
+          >
             <Stack flex shrink direction="column">
               <Stack flex shrink>
                 <Text type="white" size="large">
@@ -123,7 +159,13 @@ const Hero = ({ img, pattern }) => {
                 </Button>
               </Stack>
             </Stack>
-            <Stack flex shrink justify="end" align="center">
+            <Stack
+              flex
+              shrink
+              justify="start"
+              align="center"
+              mediumMobile={{ justify: 'end', align: 'center' }}
+            >
               <Heading inverted type="title2" element="h2">
                 <StyledAlign>
                   <StyledDate>10 may</StyledDate> ljubljana
@@ -132,7 +174,7 @@ const Hero = ({ img, pattern }) => {
             </Stack>
           </Stack>
         </BottomTextWrapper>
-      </Grid>
+      </StyledStack>
     </StyledHero>
   );
 };
