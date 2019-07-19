2213ab72161cd21595ae78fdc4098f7e6aeb04e9
william.kolmacka <william.kolmacka@kiwi.com>
2019-07-19 12:38:41 +0200

    Improved Footer responsivity

--- components/src/Footer/index.js
+++ components/src/Footer/index.js
@@ -1,10 +1,12 @@
 // @flow
 
 import * as React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Text from '@kiwicom/orbit-components/lib/Text';
 import ButtonLink from '@kiwicom/orbit-components/lib/ButtonLink';
 import Stack from '@kiwicom/orbit-components/lib/Stack';
+import Hide from '@kiwicom/orbit-components/lib/Hide';
+import media from '@kiwicom/orbit-components/lib/utils/mediaQuery';
 import Facebook from '@kiwicom/orbit-components/lib/icons/Facebook';
 import Instagram from '@kiwicom/orbit-components/lib/icons/Instagram';
 import Twitter from '@kiwicom/orbit-components/lib/icons/Twitter';
@@ -15,53 +17,98 @@ import defaultTheme from '../defaultTheme';
 
 const StyledFooter = styled.div`
   width: 100%;
-  display: flex;
-  justify-content: space-between;
-  padding: 32px;
-  border-radius: 24px 24px 0 0;
-  background: #252a31;
+  padding-right: 2.32vw;
   box-sizing: border-box;
 `;
 
-StyledFooter.defaultProps = {
+const FooterWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-basis: 110%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2.32vw;
+  border-radius: 24px 24px 0 0;
+  background: #252a31;
+  box-sizing: border-box;
+
+  ${media.largeMobile(css`
+    border-radius: 0 24px 0 0;
+  `)};
+`;
+
+FooterWrapper.defaultProps = {
   theme: defaultTheme,
 };
 
-const Logo = styled.img``;
+const StyledText = styled.span`
+  margin-right: 0.63vw;
+`;
 
-Logo.defaultProps = {
-  theme: defaultTheme,
-};
+const Logo = styled.img`
+  height: 3.16vw;
+  min-height: 40px;
+`;
 
 const Footer = () => {
   return (
-    <Stack
-      align="center"
-      justify="end"
-      basis="auto"
-      spacing="comfy"
-      grow={false}
-      shrink
-    >
-      <StyledFooter>
-        <Logo height="60px" src={KiwiLogo} />
-        <Stack align="center" justify="end" basis="auto" grow={false} shrink>
-          <Text type="white">Share us with your friends</Text>
-          <Stack justify="end" basis="0" grow={false} shrink spacing="none">
-            <ButtonLink iconLeft={<Facebook />} />
-            <ButtonLink iconLeft={<Instagram />} />
-            <ButtonLink iconLeft={<Twitter />} />
-            <ButtonLink iconLeft={<Linkedin />} />
+    <StyledFooter>
+      <Stack
+        direction="column-reverse"
+        align="center"
+        justify="end"
+        spacing="compact"
+        grow={false}
+        largeDesktop={{ direction: 'row' }}
+        shrink
+      >
+        <FooterWrapper>
+          <Logo src={KiwiLogo} />
+          <Stack
+            justify="end"
+            align="center"
+            grow={false}
+            spacing="none"
+            shrink
+          >
+            <Hide on={['smallMobile', 'mediumMobile', 'largeMobile']}>
+              <StyledText>
+                <Text size="small" type="white">
+                  Share us with your friends
+                </Text>
+              </StyledText>
+            </Hide>
+            <ButtonLink iconLeft={<Facebook />} transparent />
+            <ButtonLink iconLeft={<Instagram />} transparent />
+            <ButtonLink iconLeft={<Twitter />} transparent />
+            <ButtonLink iconLeft={<Linkedin />} transparent />
           </Stack>
+        </FooterWrapper>
+        <Stack
+          direction="column"
+          align="center"
+          basis="auto"
+          grow={false}
+          spacing="none"
+          largeMobile={{ direction: 'row' }}
+          largeDesktop={{ justify: 'between' }}
+          shrink
+        >
+          <ButtonLink size="small" type="secondary">
+            Privacy Policy
+          </ButtonLink>
+          <ButtonLink size="small" type="secondary">
+            Code of Conduct
+          </ButtonLink>
+          <ButtonLink size="small" type="secondary">
+            Legal
+          </ButtonLink>
+          <ButtonLink size="small" type="secondary">
+            Site Map
+          </ButtonLink>
         </Stack>
-      </StyledFooter>
-      <Stack align="center" basis="auto" grow={false} spacing="comfy" shrink>
-        <ButtonLink type="secondary">Privacy Policy</ButtonLink>
-        <ButtonLink type="secondary">Code of Conduct</ButtonLink>
-        <ButtonLink type="secondary">Legal</ButtonLink>
-        <ButtonLink type="secondary">Site Map</ButtonLink>
       </Stack>
-    </Stack>
+    </StyledFooter>
   );
 };
 
