cee1585d25ca65174f24df677f78dbdcdc688b1a
williamkolmacka <william.kolmacka@kiwi.com>
2019-07-23 14:23:26 +0200

    NEW: Sponsors component (#10)

---
+++ components/src/Sponsors/index.js
@@ -0,0 +1,83 @@
+// @flow
+
+import * as React from 'react';
+import styled, { css } from 'styled-components';
+import Text from '@kiwicom/orbit-components/lib/Text';
+import Stack from '@kiwicom/orbit-components/lib/Stack';
+import media from '@kiwicom/orbit-components/lib/utils/mediaQuery';
+
+import defaultTheme from '../defaultTheme';
+
+const StyledSponsors = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly
+  width: 100%;
+  padding: 5.26vw;
+  box-sizing: border-box;
+
+  ${media.tablet(css`
+    flex-direction: row;
+    align-items: center;
+  `)};
+`;
+
+StyledSponsors.defaultProps = {
+  theme: defaultTheme,
+};
+
+const StyledText = styled.span`
+  ${media.tablet(css`
+    margin-right: 4.21vw;
+  `)};
+`;
+
+StyledText.defaultProps = {
+  theme: defaultTheme,
+};
+
+const SponsorWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  margin: 4vh 0;
+  padding: 0 3.37vw;
+  box-sizing: border-box;
+
+  ${media.tablet(css`
+    width: 16.66%;
+  `)};
+`;
+
+SponsorWrapper.defaultProps = {
+  theme: defaultTheme,
+};
+
+const SponsorLogo = styled.img`
+  max-width: stretch;
+  max-height: 50px;
+`;
+
+const Sponsors = ({ logos }) => {
+  return (
+    <StyledSponsors>
+      <StyledText>
+        <Text size="large" type="secondary">
+          Sponsors
+        </Text>
+      </StyledText>
+      <Stack align="center" spacing="none" shrink wrap>
+        {logos.map((logo, index) => {
+          return (
+            <SponsorWrapper key={index} index={index}>
+              <SponsorLogo src={logo} />
+            </SponsorWrapper>
+          );
+        })}
+      </Stack>
+    </StyledSponsors>
+  );
+};
+
+export default Sponsors;
--- components/src/index.js
+++ components/src/index.js
@@ -1,5 +1,11 @@
 // @flow
 
+export { default as Faq } from './Faq';
+export { default as Footer } from './Footer';
 export { default as Hero } from './Hero';
+export { default as ImageWall } from './ImageWall';
 export { default as JoinUs } from './JoinUs';
 export { default as Mission } from './Mission';
+export { default as NavBar } from './NavBar';
+export { default as Panel } from './Panel';
+export { default as Sponsors } from './Sponsors';
